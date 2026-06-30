1a224d073ffb04381560944894fd478467e9fb46
Yadav-Aayansh <aayanshyadav@proton.me>
2026-06-30 12:54:37 +0530

    Initial project structure

---
+++ src/test/java/com/noctivagous/ledgerpay/LedgerpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noctivagous.ledgerpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
